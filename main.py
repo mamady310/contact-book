4cbc4e5d79e4b2b650d9a8454f6014c0e2d6cb05
Mamady Sillah <supurpisces310@msn.com>
2020-01-17 08:48:41 -0500

    created function to create contact

--- main.py
+++ main.py
@@ -13,7 +13,33 @@ class Contact(BaseModel):
     phone_number = CharField()
 
 db.connect()
-  chad = Contact (
-    first_name = "Chad",
-    last_name = "Smith",
-    phone_number = "7576890342",)
+db.create_tables([Contact])
+
+def create_contact(): 
+    new_first_name = input("first name ")
+    new_last_name = input("last name ")
+    new_phone_number = input("phone number ")
+
+    add_contact = Contact(
+        first_name = new_first_name,
+        last_name = new_last_name,
+        phone_number = new_phone_number
+    )
+    add_contact.save()
+   
+
+   
+
+john = Contact(
+    first_name="John", 
+    last_name="Doe", 
+    phone_number="1234567890")
+
+john.save()
+# chad = Contact (
+#  first_name = "Chad",
+#  last_name = "Smith",
+#  phone_number = "7576890342")
+
+    
+create_contact()
