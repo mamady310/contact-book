215c6ed6f77c680e8dfd1b1ea34ca0b97f4171bf
Mamady Sillah <supurpisces310@msn.com>
2020-01-17 11:08:54 -0500

    defined a function for the welcome page that gives users options on what functions they would like to call

--- main.py
+++ main.py
@@ -13,7 +13,17 @@ class Contact(BaseModel):
     phone_number = CharField()
 
 db.connect()
+db.drop_tables([Contact])
 db.create_tables([Contact])
+
+def welcome_page():
+    print("Welcome to your contact book. What would you like do:" )
+    print('show contact: 1 create contact: 2 delete contact: 3 update contact: 4 exit: 5)
+    answer = input('')
+    if answer == '1':
+        show_contact()
+    elif answer == '2':
+        create_contact()    
 
 def create_contact(): 
     new_first_name = input("first name ")
@@ -26,20 +36,38 @@ def create_contact():
         phone_number = new_phone_number
     )
     add_contact.save()
-   
 
-   
+def delete_contact():
+    first_name = input("first name ")
+    contact = Contact.get(Contact.first_name == first_name)
+    contact.delete_instace()
+    delete_contact()
 
-john = Contact(
-    first_name="John", 
-    last_name="Doe", 
-    phone_number="1234567890")
+# GO  BACK TO UPDATE LATER
+# def update_contact():
+#     update_first_name = input("first name ")  
+#     update_last_name = input("last name ")
+#     update_phone_number = input("phone number ")   
 
-john.save()
+#     edit_contact = Contact(
+#        first_name = update_first_name,
+#        last_name = update_last_name,
+#        phone_numbert = update_phone_number 
+#     )
+
+
+
+# john = Contact(
+#     first_name="John", 
+#     last_name="Doe", 
+#     phone_number="1234567890")
+
+# john.save()
+
 # chad = Contact (
 #  first_name = "Chad",
 #  last_name = "Smith",
 #  phone_number = "7576890342")
 
-    
-create_contact()
+   
+welcome_page()
